ffe64a138a4494c0f671e8298fcabce70677edfa
Santiago Nuñez <tau150@hotmail.com>
2024-02-12 20:58:58 +0100

    more on sorting

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -68,4 +68,83 @@ function merge(left, right) {
 
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+
+function insertionSort(nums){
+  for(let i = 1; i < nums.length; i ++){
+    let numberToInsert = nums[i]
+    let j;
+
+    for(j = i -1; nums[j] > numberToInsert && j >= 0; j--) {
+      nums[j + 1] = nums[j]
+    }
+
+    nums[j + 1] = numberToInsert
+  }
+
+ return nums
+}
+
+
+function quickSort(nums) {
+  // base case, arrays of length 0 or 1 are sorted already
+  if (nums.length <= 1) return nums;
+
+  // last number is the pivot
+  const pivot = nums[nums.length - 1];
+  const left = [];
+  const right = [];
+
+  // sort all smaller numbers than the pivot into left
+  // and all bigger numbers into right
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)]
+
+}
+
+
+function getDigit(number, place, longestNumber) {
+  const string = number.toString();
+  const size = string.length;
+
+  const mod = longestNumber - size;
+  return string[place - mod] || 0;
+}
+
+function findLongestNumber(array) {
+  let longest = 0;
+  for (let i = 0; i < array.length; i++) {
+    const currentLength = array[i].toString().length;
+    longest = currentLength > longest ? currentLength : longest;
+  }
+  return longest;
+}
+
+function radixSort(array) {
+  const longestNumber = findLongestNumber(array);
+
+  const buckets = new Array(10).fill().map(() => []); // make an array of 10 arrays
+
+  for (let i = longestNumber - 1; i >= 0; i--) {
+    while (array.length) {
+      const current = array.shift();
+      buckets[getDigit(current, i, longestNumber)].push(current);
+    }
+
+    for (let j = 0; j < 10; j++) {
+      while (buckets[j].length) {
+        array.push(buckets[j].shift());
+      }
+    }
+  }
+
+  return array;
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge, insertionSort, bubbleSortWithWhile, quickSort, radixSort };
